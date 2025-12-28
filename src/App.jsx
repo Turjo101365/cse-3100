0765384c3ce8a4c8bd47a5877b1b42f1760ae4cb
Turjo101365 <acd776959@gmail.com>
2025-12-29 01:03:41 +0600

    Remove type module from package.json for Netlify compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
-import BaseLayout from './views/BaseLayout';
-import Home from './views/Home';
-import About from './views/About';
-import AvailableCats from './views/AvailableCats';
-import Contact from './views/Contact';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Import all views
+import BaseLayout from "./views/BaseLayout";
+import Home from "./views/Home";
+import About from "./views/About";
+import AvailableCats from "./views/AvailableCats";
+import Contact from "./views/Contact";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<BaseLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/available-cats" element={<AvailableCats />} />
-        <Route path="/contact" element={<Contact />} />
-      </Route>
-    </Routes>
+    <Router>
+      <Routes>
+        {/* BaseLayout wraps all routes */}
+        <Route element={<BaseLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/available-cats" element={<AvailableCats />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
